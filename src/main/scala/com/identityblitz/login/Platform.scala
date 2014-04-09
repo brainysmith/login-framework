31cab09041a41e5958d0f616067e2a9b6a8d5cc2
vkarpov <brainysmith@gmail.com>
2014-04-09 15:06:46 +0400

    enumeration

---
+++ src/main/scala/com/identityblitz/login/Platform.scala
@@ -0,0 +1,10 @@
+package com.identityblitz.login
+
+/**
+ * The enumeration of platform supported by the login framework.
+ */
+object Platform extends Enumeration {
+  type Platform = Value
+  val SERVLET, PLAY = Value
+}
+
