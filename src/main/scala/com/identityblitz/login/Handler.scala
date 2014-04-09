a92589285b842ca60e6384eb460889071d82ecde
vkarpov <brainysmith@gmail.com>
2014-04-09 18:39:14 +0400

    AP servlet

---
+++ src/main/scala/com/identityblitz/login/Handler.scala
@@ -0,0 +1,11 @@
+package com.identityblitz.login
+
+import com.identityblitz.login.transport.{OutboundTransport, InboundTransport}
+import com.identityblitz.login.error.LoginException
+
+trait Handler {
+
+  @throws(classOf[LoginException])
+  def start(itr: InboundTransport, otr: OutboundTransport)
+
+}
